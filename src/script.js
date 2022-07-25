20cdb76ccef65a1e103e99aae587bf952f012247
Gabriel Araújo <101131979+gabri-1910@users.noreply.github.com>
2022-07-25 15:37:17 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,12 +1,34 @@
 function Calcular() {
   // Primeiro passo é definir as variáveis e alocar os valores do input.
-  var n1 = parseFloat(document.getElementById("valorRig").value);
-  var n2 = parseFloat(document.getElementById("valorTotal").value);
-  var n3 = parseFloat(document.getElementById("valorTotal").value);
+  var n1 = parseFloat(document.getElementById("notaBi1").value);
+  var n2 = parseFloat(document.getElementById("notaBi2").value);
+  var n3 = parseFloat(document.getElementById("notaBi3").value);
+  var n4 = parseFloat(document.getElementById("notaBi4").value);
 
-  //Agora vamos calcular o quanto temos de pagar da conta de luz.
+  var media = (n1 + n2 + n3 + n4) / 4;
+  var notaFixada = media.toFixed(1);
 
-  var valorConvertido = document.getElementById("valorConvertido");
-  var valorFinalConvertido = "O valor em real é: R$ " + (n1 / n2) * n3;
-  valorConvertido.innerHTML = valorFinalConvertido;
+  // Testando se os campos estao preenchidos e se a media é menor que 7
+  if (
+    document.getElementById("notaBi1") !== "" &&
+    document.getElementById("notaBi2") !== "" &&
+    document.getElementById("notaBi3") !== "" &&
+    document.getElementById("notaBi4") !== "" &&
+    notaFixada < 6
+  ) {
+    var elementoMedia = document.getElementById("resultado");
+    var resultadoMedia = "Sua média foi de " + notaFixada + ". Você reprovou";
+    elementoMedia.innerHTML = resultadoMedia;
+
+  } else if (notaFixada > 6) {
+    var elementoMedia = document.getElementById("resultado");
+    var resultadoMedia =
+      "Sua média foi de " + notaFixada + ". Você Foi Aprovado!";
+    elementoMedia.innerHTML = resultadoMedia;
+
+  } else {
+    var elementoErro = document.getElementById("resultado");
+    var erro = "Preencha todos os campos";
+    elementoErro.innerHTML = erro;
+  }
 }
