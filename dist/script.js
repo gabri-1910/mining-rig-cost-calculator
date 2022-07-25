7325704c16af99bfb27dfac4fa6a5e79c0dbeb46
Gabriel Araújo <101131979+gabri-1910@users.noreply.github.com>
2022-07-25 15:38:18 -0300

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -2,11 +2,14 @@ function Calcular() {
   // Primeiro passo é definir as variáveis e alocar os valores do input.
   var n1 = parseFloat(document.getElementById("valorRig").value);
   var n2 = parseFloat(document.getElementById("valorTotal").value);
-  var n3 = parseFloat(document.getElementById("valorTotal").value);
+  var n3 = parseFloat(document.getElementById("contaTotal").value);
 
   //Agora vamos calcular o quanto temos de pagar da conta de luz.
 
+  var soma = n1 / n2;
+  var final = soma * n3;
+
   var valorConvertido = document.getElementById("valorConvertido");
-  var valorFinalConvertido = "O valor em real é: R$ " + (n1 / n2) * n3;
+  var valorFinalConvertido = "O valor em real é: R$ " + final;
   valorConvertido.innerHTML = valorFinalConvertido;
 }
